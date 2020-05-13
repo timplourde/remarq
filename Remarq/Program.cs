01bf8a515afce91afb6646f343a9c656a96dbfda
Tim Plourde <tplourde@gmail.com>
2020-05-12 21:37:59 -0400

    adds exception handling

--- Remarq/Program.cs
+++ Remarq/Program.cs
@@ -15,10 +15,18 @@ namespace Remarq
             var source = args[0];
             var target = args[1];
             var template = args[2];
-            var generator = new Generator(source, target, template);
-            var result = await generator.Generate();
-            Console.WriteLine($"Wrote {result} files from {source} to {target}");
-            return 0;
+            try
+            {
+                var generator = new Generator(source, target, template);
+                var result = await generator.Generate();
+                Console.WriteLine($"Wrote {result} files from {source} to {target}");
+                return 0;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+                return 1;
+            }
         }
     }
 }
